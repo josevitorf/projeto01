b2231ac222e569b53f358c2461f86ab86c75e5df
Jose Vitor <josevitor.f@hotmail.com>
2021-02-20 12:37:01 -0300

    primeiro commit do projeto

---
+++ src/test/java/com/projeto01/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
